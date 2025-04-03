ad47368a1b2dfa7ca2bec124c383e330d9323667
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-04-03 17:33:04 +0200

    Formatter fixes

--- src/Components.jsx
+++ src/Components.jsx
@@ -1,50 +1,50 @@
 import "./index.css";
 
 const initialItems = [
-	{ id: 1, description: "Passports", quantity: 2, packed: false },
-	{ id: 2, description: "Socks", quantity: 12, packed: false },
-	{ id: 3, description: "Charger", quantity: 1, packed: true },
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export function Logo() {
-	return <h1>Far Away</h1>;
+  return <h1>Far Away</h1>;
 }
 
 export function Form() {
-	return (
-	<div className="add-form">
-		<h3>What do you need for your trip?</h3>
-	</div>
-	);
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
 }
 
 export function PackingList() {
-	return (
-		<div className="list">
-			<ul>
-				{initialItems.map((item) => (<Item item={item} />))}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function Item({ item }) {
-	return (
-		<li>
-			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
-				{item.quantity} {item.description}
-			</span>
-			<button className="remove">❌</button>
-		</li>
-	);
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button className="remove">❌</button>
+    </li>
+  );
 }
 
 export function Stats() {
-	return (
-		<footer className="stats">
-			<em>
-				You have x items on your list, and you already packed x items
-			</em>
-		</footer>
-	);
+  return (
+    <footer className="stats">
+      <em>You have x items on your list, and you already packed x items</em>
+    </footer>
+  );
 }
